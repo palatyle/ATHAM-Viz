8486c355a3f11a06a35cf150086526e3093895a6
Tyler Paladino <palatyle@isu.edu>
2022-08-01 09:19:46 -0600

    Added in lat loop properly

--- ATHAM_viz_multi.m
+++ ATHAM_viz_multi.m
@@ -1,46 +1,47 @@
 function ATHAM_viz_multi(tracer_name,isovalue,domain_flux,upper_dir,vent_diam,lat,small_grid,density_overlay, quiver_overlay, dep_calc, passwd)
-for j = 1:length(vent_diam)
-    tic
-    % tracer_name = 'ash1';
-    % isovalue = .001;
-    % domain_flux = false;
-    % small_grid = false;
-    % upper_dir = '/Volumes/(M)ATHAM/';
-    % vent_diam = '75m';
-    % lat = 'polar';
-    dir_info=dir(fullfile(strcat(upper_dir,lat,'/',vent_diam(j)),'**/atham_netCDF_MOV.nc'));
-    dirs = extractfield(dir_info,'folder')';
+for k = 1:length(lat)
+    for j = 1:length(vent_diam)
+        tic
+        % tracer_name = 'ash1';
+        % isovalue = .001;
+        % domain_flux = false;
+        % small_grid = false;
+        % upper_dir = '/Volumes/(M)ATHAM/';
+        % vent_diam = '75m';
+        % lat = 'polar';
+        dir_info=dir(fullfile(strcat(upper_dir,lat(k),'/',vent_diam(j)),'**/atham_netCDF_MOV.nc'));
+        dirs = extractfield(dir_info,'folder')';
 
-    output = {'Vent speed (m/s)','Wind Speed (m/s)','stability mean','stability med','stability SD','Max plume height (km)','Neutral Buoyancy Height (km)', 'NBH err (km)'};
-    for i = 1:length(dirs)
-        fn = dirs{i};
-        [flux_ratio_mean, flux_ratio_med, flux_ratio_SD, max_plume_height, NBH, NBH_err] = ATHAM_viz_ts(fn, tracer_name, isovalue, domain_flux, small_grid, density_overlay, quiver_overlay, dep_calc);
-        split_dir = regexp(fn,filesep,'split');
-        output_fn = split_dir{end};
+        output = {'Vent speed (m/s)','Wind Speed (m/s)','stability mean','stability med','stability SD','Max plume height (km)','Neutral Buoyancy Height (km)', 'NBH err (km)'};
+        for i = 1:length(dirs)
+            fn = dirs{i};
+            [flux_ratio_mean, flux_ratio_med, flux_ratio_SD, max_plume_height, NBH, NBH_err] = ATHAM_viz_ts(fn, tracer_name, isovalue, domain_flux, small_grid, density_overlay, quiver_overlay, dep_calc);
+            split_dir = regexp(fn,filesep,'split');
+            output_fn = split_dir{end};
 
-        split_fn = regexp(output_fn,'_','split');
-        vent_speed = split_dir{4};
+            split_fn = regexp(output_fn,'_','split');
+            vent_speed = split_dir{4};
 
-        if split_dir{3} == "127_5m"
-            wind_speed = split_fn{5};
-        else
-            wind_speed = split_fn{4};
+            if split_dir{3} == "127_5m"
+                wind_speed = split_fn{5};
+            else
+                wind_speed = split_fn{4};
+            end
+            output{i+1,1} = vent_speed;
+            output{i+1,2} = wind_speed;
+            output{i+1,3} = flux_ratio_mean;
+            output{i+1,4} = flux_ratio_med;
+            output{i+1,5} = flux_ratio_SD;
+            output{i+1,6} = max_plume_height;
+            output{i+1,7} = NBH;
+            output{i+1,8} = NBH_err;
         end
-        output{i+1,1} = vent_speed;
-        output{i+1,2} = wind_speed;
-        output{i+1,3} = flux_ratio_mean;
-        output{i+1,4} = flux_ratio_med;
-        output{i+1,5} = flux_ratio_SD;
-        output{i+1,6} = max_plume_height;
-        output{i+1,7} = NBH;
-        output{i+1,8} = NBH_err;
+        cd(upper_dir)
+        writecell(output,strcat(lat(k),'_',vent_diam(j),'.txt'))
+        matlabmail('tylerpaladino@isu.edu',strcat(vent_diam(j), ' Done'),'Script Finished','palatyle@isu.edu',passwd)
+        disp(strcat('Done visualizing vent ',vent_diam(j),'in:'))
+        toc
     end
-    cd(upper_dir)
-    writecell(output,strcat(lat,'_',vent_diam(j),'.txt'))
-    matlabmail('tylerpaladino@isu.edu',strcat(vent_diam(j), ' Done'),'Script Finished','palatyle@isu.edu',passwd)
-    disp(strcat('Done visualizing vent ',vent_diam(j),'in:'))
-    toc
 end
-
 end
 
